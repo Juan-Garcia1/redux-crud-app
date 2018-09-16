256e7676021266005f4c814b04a79d7c9cb81cee
Juan Garcia <juan_garcia_1995@hotmail.com>
2018-09-16 15:25:34 -0500

    added redux dev tools extension

--- src/store.js
+++ src/store.js
@@ -1,7 +1,13 @@
-import { createStore } from "redux"
+import { createStore, compose } from "redux"
 import rootReducer from "./reducer"
 const initialState = {}
 
-const store = createStore(rootReducer, initialState)
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 export default store
